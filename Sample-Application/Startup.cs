5ec731e00cb7090cec3ad268e4c7a419772c7512
apupetkar <apu.petkar@gmail.com>
2016-06-09 00:26:22 +0200

    test

---
+++ Sample-Application/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sample_Application.Startup))]
+namespace Sample_Application
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
